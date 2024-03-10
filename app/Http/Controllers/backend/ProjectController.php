039a65953ad20a97ea36d8d573bdc9e1b4ae0281
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-03-10 12:37:48 +0600

    project and skill added to navigation

--- app/Http/Controllers/backend/ProjectController.php
+++ app/Http/Controllers/backend/ProjectController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Projects;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index(){
+        $projects=Projects::all();
+        return view('projects.index',compact('projects'));
+    }
 }
--- app/Http/Controllers/backend/SkillController.php
+++ app/Http/Controllers/backend/SkillController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Skills;
 use Illuminate\Http\Request;
 
 class SkillController extends Controller
 {
-    //
+       public function index(){
+        $skills=Skills::all();
+        return view('skills.index',compact('skills'));
+    }
 }
