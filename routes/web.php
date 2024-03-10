bbea8b3f2df8388b94a7b8eacdf3e43ac0edd470
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-03-10 10:17:27 +0600

    created skill and project controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\backend\ProjectController;
+use App\Http\Controllers\backend\SkillController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('/project',ProjectController::class);
+    Route::resource('/skill',SkillController::class);
 });
 
 require __DIR__.'/auth.php';
